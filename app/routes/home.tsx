3b347f4c798e7eacee5b682017138a4a59a6e48f
onozaty <onozaty@gmail.com>
2025-06-29 00:16:26 +0900

    トップページにbutton配置

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
+import { Button } from '~/components/ui/button';
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +9,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }
