7bf135be476b3d789a35f94eca443bb5ce74c833
onozaty <onozaty@gmail.com>
2025-07-13 22:27:04 +0900

    react-router/serve を使わずに、react-router/express を使う方法に(この後、ログなど変えるため)
    https://github.com/remix-run/react-router-templates/tree/main/node-custom-server

---
+++ server/app.ts
@@ -0,0 +1,14 @@
+import { createRequestHandler } from "@react-router/express";
+import express from "express";
+import "react-router";
+
+export const app = express();
+
+app.use(
+  createRequestHandler({
+    build: () => import("virtual:react-router/server-build"),
+    getLoadContext() {
+      return new Map();
+    },
+  }),
+);
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,16 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: "./server/app.ts",
+        }
+      : undefined,
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
     host: "127.0.0.1",
   },
-});
+}));
